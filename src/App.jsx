888e4c5f92fcbb3f335a9ddf9598e0e34ba14296
Truongxt <truongxt2501@gmail.com>
2025-03-07 19:45:14 +0700

    boostrap

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,11 @@ import AdminLogin from './pages/AdminLogin'
 import About from './pages/About'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 import './App.css'
+import Resgister from './pages/Resgister'
 function App() {
 
   return (
@@ -17,6 +21,7 @@ function App() {
           <Route path='/contact' element={<Contact/>}/>
           <Route path='/adminlogin' element={<AdminLogin/>}/>
           <Route path='/about' element={<About/>}/>
+          <Route path='/register' element={<Resgister/>}/>
         </Routes>
         </div>
       <Footer></Footer>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,38 @@
-import {NavLink} from 'react-router-dom'
-import airplane from '../img/plane.png'
-import '../../css/header.css'
-import userLogo from '../img/user.png'
+import { NavLink } from "react-router-dom";
+import { Navbar, Nav, Container, Button } from "react-bootstrap";
+import airplane from "../img/plane.png";
+import userLogo from "../img/user.png";
+import "bootstrap/dist/css/bootstrap.min.css";
 const Header = () => {
     return (
         <div className='header'>
-            <nav>
-            <NavLink to="/" className='logo'><img src={airplane} alt="" width={40}/>Travel Booking</NavLink>
-                <ul>
-                    <li>
-                        <NavLink to="/">Trang chủ</NavLink>
-                    </li>
-                    <li>
-                        <NavLink>Vé máy bay</NavLink>
-                    </li>
-                    <li>
-                        <NavLink>Vé xe khách</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/contact">Liên hệ</NavLink>
-                    </li>
-                    <li>
-                        <NavLink to="/about">About</NavLink>
-                    </li>
-                </ul>
-            </nav>
-            <div className='user'>
-                <NavLink to="/adminlogin" className="loginLogo"><img src={userLogo} alt="" width={20}/>Đăng nhập</NavLink>
-                <NavLink  className="registerLogo">Đăng ký</NavLink>
-            </div>
-        </div>
+            <Navbar expand="lg" bg="primary" className="py-3">
+        
+                <Container>
+                    <Navbar.Brand as={NavLink} to="/" className="d-flex align-items-center">
+                        <img src={airplane} alt="logo" width={40} className="me-2" />
+                        <span className="fs-5 fw-bold text-white">Travel Booking</span>
+                    </Navbar.Brand>
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ms-auto">
+                            <Nav.Link as={NavLink} to="/" className="text-white">Trang chủ</Nav.Link>
+                            <Nav.Link as={NavLink} to="#" className="text-white">Vé máy bay</Nav.Link>
+                            <Nav.Link as={NavLink} to="#" className="text-white">Vé xe khách</Nav.Link>
+                            <Nav.Link as={NavLink} to="/contact" className="text-white">Liên hệ</Nav.Link>
+                            <Nav.Link as={NavLink} to="/about" className="text-white">About</Nav.Link>
+                        </Nav>
+                        <div className="d-flex align-items-center ms-lg-3 gap-3">
+                            <Button as={NavLink} to="/adminlogin" variant="light" className="d-flex align-items-center">
+                                <img src={userLogo} alt="user" width={20} className="me-2" /> Đăng nhập
+                            </Button>
+                            <Button as={NavLink} to="/register" variant="warning">Đăng ký</Button>
+                        </div>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+
+        </div >
     );
 
 }
